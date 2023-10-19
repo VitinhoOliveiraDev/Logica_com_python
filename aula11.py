07da1f980ecd74e5d896f9614e143eb2d156e1f5
Victor Paulo Oliveira De Souza <vitorpaulo.pvc22@outlook.com>
2023-10-18 22:08:52 -0300

    curso

---
+++ aula11.py
@@ -0,0 +1,16 @@
+# Expressões Logicas
+
+idade = 18
+carteiraMotorista = True
+
+print(idade >= 18 and carteiraMotorista == True)
+
+print("Pode dirigir")
+
+velocidade = 90
+radar = 100
+rodarFuncionando = False
+
+print(velocidade > radar and radarfuncionando == True)
+
+print("Não foi multado")
---
+++ variáveis/exercicio_aula1/exercicio6.py
@@ -0,0 +1,20 @@
+# Exercicio
+
+aluno1 = 5
+aluno2 = 7
+aluno3 = 10
+media = 7
+
+print(aluno1 >= media)
+print(aluno2 >= media)
+print(aluno3 >= media)
+
+prova_a = 5 
+prova_b = 8
+prova_c = 10
+
+media_provas = (prova_a + prova_b + prova_c) / 3
+
+print(media_provas)
+
+print(media_provas >= media)
